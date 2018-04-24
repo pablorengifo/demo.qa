c7721f1c4a76e33d58cecfe9cd2b1768fa72122b
Pablo Rengifo <prengifos@gmail.com>
2018-04-24 12:57:48 -0500

    adding search criteria

--- src/main/java/com/example/demoqa/WebDriverTest.java
+++ src/main/java/com/example/demoqa/WebDriverTest.java
@@ -2,8 +2,12 @@ package com.example.demoqa;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -17,8 +21,15 @@ public class WebDriverTest {
 
     @Test
     public void testEbay() {
-        WebDriver webDriver =  new FirefoxDriver();
-        webDriver.navigate().to("https://www.ebay.com/");
-        assertThat("ebay",webDriver.getTitle().contains("eBay"),is(Boolean.TRUE));
+        WebDriver driver =  new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.navigate().to("https://www.ebay.com/");
+        assertThat("ebay",driver.getTitle().contains("eBay"),is(Boolean.TRUE));
+
+        driver.findElement(By.xpath("//input[@id='gh-ac']")).sendKeys("shoes puma");
+
+        driver.findElement(By.xpath("//input[@id='gh-btn']")).click();
+
+                //driver.findElement(By.id("gh-ac"));
     }
 }
