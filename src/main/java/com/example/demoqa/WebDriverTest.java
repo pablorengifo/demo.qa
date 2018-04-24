bf4cdcff96c02b56727c7bcd63e8b06886e31f0b
Pablo Rengifo <prengifos@gmail.com>
2018-04-24 11:40:46 -0500

    Add files via upload

---
+++ src/main/java/com/example/demoqa/WebDriverTest.java
@@ -0,0 +1,24 @@
+package com.example.demoqa;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class WebDriverTest {
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver", "linux/geckodriver");
+    }
+
+    @Test
+    public void testEbay() {
+        WebDriver webDriver =  new FirefoxDriver();
+        webDriver.navigate().to("https://www.ebay.com/");
+        assertThat("ebay",webDriver.getTitle().contains("eBay"),is(Boolean.TRUE));
+    }
+}
